ff76fb27d62484832e1dde7ea9b9d2d740ed4be9
hagbarth <emil@hivebeat.com>
2015-08-28 14:00:20 +0200

    fix main component

--- js/components/Main.js
+++ js/components/Main.js
@@ -1,19 +1,13 @@
-// import React, { Component, PropTypes } from 'react';
-// import { SOME_ACTION } from '../constants/ActionTypes';
+import React, { Component } from 'react';
 
-// export default class Main extends Component {
-//   static propTypes = {
-//     someapp: PropTypes.object.isRequired,
-//     actions: PropTypes.object.isRequired
-//   }
+export default class Main extends Component {
+  constructor() {
 
-//   constructor() {
+  }
 
-//   }
-
-//   render() {
-//     return (
-//       <div>Main componentSome</div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>Main componentSome</div>
+    );
+  }
+}
