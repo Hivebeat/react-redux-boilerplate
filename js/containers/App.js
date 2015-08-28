197627fcffc581f8f28a0c667adcba2529a858b9
hagbarth <emil@hivebeat.com>
2015-08-28 21:07:14 +0200

    Fix app to use redux for init

--- js/containers/App.js
+++ js/containers/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import SomeApp from './SomeApp';
-import { createStore, combineReducers } from 'redux';
+import ImportEventApp from './ImportEventApp';
+import create from '../redux';
 import { Provider } from 'react-redux';
 import * as reducers from '../reducers';
 
 const reducer = combineReducers(reducers);
 const store = createStore(reducer);
 
-export default class App extends Component {
+export default class App {
   render() {
-    return (
-      <Provider store={store}>
-        {() => <SomeApp /> }
-      </Provider>
-    );
-  }
+      var store = null;
+      if (typeof window !== 'undefined') {
+            store = create(reducers, window.__data);
+          } else {
+                store = create(reducers);
+              }
+      return (
+            <Provider store={store}>
+              {() => <CommunityApp />}
+            </Provider>
+          );
+    }
 }
+
