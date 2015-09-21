1a6b06671d4d584c24ff0b6e27ec50814675a11d
hagbarth <emil@hivebeat.com>
2015-09-21 16:10:37 +0200

    generic controller

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -5,20 +5,18 @@ import App from 'path/to/app/dir/js/containers/App';
 
 module.exports = function(app) {
   app.locals.routers.www.get('/path', function (req, res) {
-      const store = importEvent.create(importEvent.reducers);
-      store.dispatch(importEvent.actions.IntlActions.loadMessages({
-            locale: app.locals.locale
-          }));
+      const store = theApp.create(theApp.reducers);
       const state = store.getState();
       res.render('template', {
             layout: 'layout',
             GLOBAL_CONFIG: JSON.stringify({
-                    LOCALE: app.locals.locale,
-                    API_HOST: app.locals.API_HOST,
-                    FB_APP_ID: app.locals.FB_APP_ID,
-                    HOST: app.locals.HOST.replace('//', '//www.'),
-                    APP_HOST: app.locals.APP_HOST
-                  }),
+              LOCALE: app.locals.locale,
+              API_HOST: app.locals.API_HOST,
+              FB_APP_ID: app.locals.FB_APP_ID,
+              MP_TOKEN: app.locals.MP_TOKEN,
+              HOST: app.locals.HOST.replace('//', '//www.'),
+              APP_HOST: app.locals.APP_HOST
+            }),
             react: React.renderToString(
                     <App store={store}/>
                   ),
@@ -26,4 +24,3 @@ module.exports = function(app) {
           });
     });
 };
-
